8166dae0f7ddbf5e8c2f1b0bfd984949c7066a1a
2tmirleid <valeriyjobemail@gmail.com>
2024-10-30 14:38:28 +0300

    v.0.0.1 - Добавил метод в BasePage для проверки целостности системных переменных

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,13 +3,17 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from pywinauto import Application
 
+from utils.enums.env_vars.env_vars_enum import EnvVarsEnum
 from utils.enums.timeouts.timeouts_enum import TimeoutsEnum
 
 class BasePage:
     def __init__(self):
         self.timeouts = TimeoutsEnum
+        self.env_vars = EnvVarsEnum
 
         load_dotenv(find_dotenv())
+
+        self.env_vars_exists()
 
         self.exe_path = os.environ["EXE_PATH"]
         self.title    = os.environ['WINDOW_TITLE']
@@ -17,10 +21,14 @@ class BasePage:
         self.app    = Application(backend="uia").start(self.exe_path)
         self.window = self.app.window(title=self.title)
 
-    def env_vars_are_exists(self):
-        #TODO Функция, которая запускается при наследовании класса и проверяет наличие переменных окружения
-        ...
+    def env_vars_exists(self) -> None:
+        for env_var in self.env_vars:
+            if not os.environ.get(str(env_var.value)):
+                raise Exception(f"Env var {env_var.value} not found")
 
-    def wait_until_visible(self, timeout: int = 10) -> None:
+
+    def wait_until_visible(self, timeout: int = None) -> None:
+        if timeout is None:
+            timeout = self.timeouts.IMPLICIT.value
+
         self.window.wait('visible', timeout=timeout)
-
---
+++ utils/enums/env_vars/env_vars_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class EnvVarsEnum(Enum):
+    EXE_PATH     = "EXE_PATH"
+    WINDOW_TITLE = "WINDOW_TITLE"
